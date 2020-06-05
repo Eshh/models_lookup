04606524a8d9e20130a10f48105ad16229ae623d
Eshh1997 <eswarprasad943@gmail.com>
2020-06-05 17:54:02 +0530

    test

--- src/components/card-list/card-list-component.jsx
+++ src/components/card-list/card-list-component.jsx
@@ -3,7 +3,6 @@ import './card-list-styles.css';
 import {Card} from '../card-component/card-component';
 
 export const CardList = (props) => {
-    console.log(props)
     return (
         <div className = 'card-list'>
            {props.heroines.map(heroine => (
